d8a62b9a7f682de214ce0bb574f06ead568726b5
Dagmar Feldt <d.feldt@t-online.de>
2024-07-26 20:45:11 +0200

    Initial Commit

---
+++ MentalUnload/MentalUnloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentalUnloadApp.swift
+//  MentalUnload
+//
+//  Created by Dagmar Feldt on 26.07.24.
+//
+
+import SwiftUI
+
+@main
+struct MentalUnloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
